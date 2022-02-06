88a9fb17d9c5f34b2374d961a88157f853e52ad5
ksans01 <kurt.sanson@bryanuniversity.edu>
2022-02-06 17:13:15 -0600

    Weather App initial

--- week6/assignment/scripts/getDaylight.js
+++ week6/assignment/scripts/getDaylight.js
@@ -1,2 +1,11 @@
 // This script should return the results of a function that use data from the api to determine whether it 
 // is daylight in the specified city. It should return "blue" if daylight, and "black" if night time.
+
+import { get } from "http";
+
+
+function getDaylight(a, b){
+    return a + b;    
+}
+
+console.log(getDaylight(4,5))
--- week6/assignment/scripts/weather.js
+++ week6/assignment/scripts/weather.js
@@ -13,3 +13,22 @@
 // create a function that writes the temperature (using local units), humidity, and conditions
 // this function should also change the background color of the weather app to blue during the daylight
 // hours at the specified city
+
+
+// import getDaylight from './getDaylight.js'
+// import converTemp from '/convertTemp.js'
+
+const weather = () => {
+    axios.get('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=86156c9a86ff37f9af32d6f59b53d6bb')
+    .then(res => console.log(res.data.list))
+    .catch(error => console.log(error))
+}
+
+weather()
+// const cors = require('cors');
+// const express = require('express');
+// const app = express();
+// app.use(cors());
+// app.options('*', cors());
+
+console.log("Hello Universe")
